6c327d5092bc217fee99ac1e92958e47a4dcb3e9
kamiuetian <kami_se01@live.com>
2024-08-02 15:29:13 +0500

    completed exercises till 1.5

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,46 +1,56 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+ const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total totalExercises={exercises1 + exercises2 + exercises3} />
-    </div>
+      <Content course={course} />
+      <Total course={course} />
+      </div>
   )
 }
 /**Header component */
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 /**Content component */
 const Content = (props) => {
   return (
     <>
-    <Part part={props.part} exercises={props.exercises} />
+    <Part part={props.course.parts[0]} />
+    <Part part={props.course.parts[1]} />
+    <Part part={props.course.parts[2]} />
     </>
   )
 }
 /**Total component */
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.totalExercises}</p>
+    <p>Number of exercises {props.course.parts[0].exercises+props.course.parts[1].exercises+props.course.parts[2].exercises}</p>
   )
 }
 /**Part component */
 const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 export default App
